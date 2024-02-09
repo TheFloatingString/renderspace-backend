75d12b55707633b6181b5a4e0f60a5f367323f19
Laurence Liang <laurence.w.liang@gmail.com>
2024-02-09 13:39:51 -0500

    Add client views

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return render_template("index.html")
+
+@app.route("/api/robot", methods=["GET", "POST"])
+def post_route_robot():
+    print(request.json)
+    return {"data": "success"}
+
+@app.route("/view/student_side")
+def view_student_side():
+    return render_template("student_side.html")
+
+@app.route("/view/company_side")
+def view_company_side():
+    return render_template("company_side.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
